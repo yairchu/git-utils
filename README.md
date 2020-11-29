fe8aa035bbc8bb1b83d03bc8a285507fcc04a183
Yair Chuchem <yairchu@gmail.com>
2020-11-29 16:40:24 +0200

    add sub-merge.sh

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # git-utils
 Small helpers for using git
+
+## sub-rebase.sh
+
+Rebase gradually: replay branch commits over newer commits on target branch, to handle the conflicts in smaller units.
+
+See post: [Break big merges to smaller pieces](https://yairchu.github.io/posts/split-merge-to-smaller-pieces)
+
+## sub-merge.sh
+
+A merging variant of `sub-rebase.sh`
---
+++ sub-merge.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BASE=${1:-master}
+for COMMIT in $(git rev-list ..$BASE --reverse)
+do
+    if git merge --no-commit $COMMIT >/dev/null 2>/dev/null; then
+        git reset --hard >/dev/null
+    else
+        echo Conflicts when merging with $COMMIT, merging with its parent
+        git reset --hard >/dev/null
+        git merge --no-edit $COMMIT^
+        exit 1
+    fi
+done
+echo No conflicts, merging!
+git merge --no-edit $BASE
