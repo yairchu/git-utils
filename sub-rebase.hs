c1310950b3b1019e0278c928fceb11d9291a8936
Yair Chuchem <yairchu@gmail.com>
2021-01-03 12:13:58 +0200

    Translate sub-rebase to Haskell

---
+++ sub-rebase.hs
@@ -0,0 +1,51 @@
+#!/usr/bin/env stack
+-- stack --resolver nightly-2021-01-03 script
+
+{-# LANGUAGE LambdaCase #-}
+
+import Control.Monad
+import Control.Lens.Operators
+import Data.Foldable.Extra
+import Data.List
+import Data.Maybe
+import qualified Options.Applicative as O
+import System.Exit
+import System.Process
+
+opts :: O.ParserInfo String
+opts =
+    O.info
+    (O.helper <*>
+        (O.optional (O.strArgument (O.metavar "BASE" <> O.help "Base branch to rebase over"))
+            <&> fromMaybe "master"))
+    (O.fullDesc <> O.progDesc "Gradually rebase to handle less merge conflicts at a time")
+
+cmd :: String -> IO String
+cmd x = readCreateProcess (shell x) ""
+
+subRebase :: String -> IO ()
+subRebase base =
+    do
+        commonAncestor <- cmd ("git merge-base HEAD " <> base)
+        let isStrictlyAhead commit =
+                cmd ("git merge-base HEAD " <> commit) <&> (== commonAncestor)
+        cmd ("git rev-list .." <> base)
+            <&> lines <&> reverse
+            >>= findM isStrictlyAhead
+            >>=
+            \case
+            Nothing -> putStrLn "Done"
+            Just commit -> do
+                putStrLn ("Rebasing over " <> commit)
+                spawnCommand ("git rebase " <> commit) >>= waitForProcess
+                    >>= (`when` subRebase base) . (== ExitSuccess)
+
+allConflictsFixed :: IO Bool
+allConflictsFixed =
+    cmd "git status" <&> isInfixOf "all conflicts fixed: run \"git rebase --continue\""
+
+main :: IO ()
+main = do
+    base <- O.execParser opts
+    allConflictsFixed >>= (`when` callCommand "git rebase --continue")
+    subRebase base
