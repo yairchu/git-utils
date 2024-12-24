54206679e675ed5ae4dcaa2531f1fe7c60cb188e
Yair Chuchem <yairchu@gmail.com>
2024-12-24 14:55:56 +0200

    sub-merge: workaround git sometimes failing due to reasons, doesn't indicate merge had conflicts

--- sub-merge.sh
+++ sub-merge.sh
@@ -5,7 +5,19 @@ DEFAULT_BRANCH=$([ -f "${GIT_FOLDER}"/refs/heads/master ] && echo master || echo
 BASE=${1:-$DEFAULT_BRANCH}
 for COMMIT in $(git rev-list .."$BASE" --reverse)
 do
-    if git merge --no-commit "$COMMIT" >/dev/null 2>/dev/null; then
+    COMMAND="git merge --no-commit $COMMIT"
+    $COMMAND >/dev/null
+    STATUS=$?
+    while [ $STATUS == 128 ]; do
+        echo "Git fatal failure (probably another program using git at the same time)"
+        echo "Applying git reset --hard and retrying"
+        echo
+        git reset --hard >/dev/null
+        $COMMAND >/dev/null
+        STATUS=$?
+    done
+    if [ $STATUS == 0 ]; then
+        echo No conflicts! git reset --hard
         git reset --hard >/dev/null
     else
         echo Conflicts when merging with "$COMMIT", merging with its parent
