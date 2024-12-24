99cfa9726d93b6b2cc9c799a30a45ddd61379db0
Yair Chuchem <yairchu@gmail.com>
2024-12-24 14:53:56 +0200

    shellcheck linting

--- sub-merge.sh
+++ sub-merge.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-GIT_FOLDER=`git rev-parse --show-toplevel`/.git
-DEFAULT_BRANCH=`[ -f ${GIT_FOLDER}/refs/heads/master ] && echo master || echo main`
+GIT_FOLDER=$(git rev-parse --show-toplevel)/.git
+DEFAULT_BRANCH=$([ -f "${GIT_FOLDER}"/refs/heads/master ] && echo master || echo main)
 BASE=${1:-$DEFAULT_BRANCH}
-for COMMIT in $(git rev-list ..$BASE --reverse)
+for COMMIT in $(git rev-list .."$BASE" --reverse)
 do
-    if git merge --no-commit $COMMIT >/dev/null 2>/dev/null; then
+    if git merge --no-commit "$COMMIT" >/dev/null 2>/dev/null; then
         git reset --hard >/dev/null
     else
-        echo Conflicts when merging with $COMMIT, merging with its parent
+        echo Conflicts when merging with "$COMMIT", merging with its parent
         git reset --hard >/dev/null
-        git merge --no-edit $COMMIT^
-        echo Merging with $COMMIT
-        git merge $COMMIT
+        git merge --no-edit "$COMMIT"^
+        echo Merging with "$COMMIT"
+        git merge "$COMMIT"
         exit 1
     fi
 done
 echo No conflicts, merging!
-git merge --no-edit $BASE
+git merge --no-edit "$BASE"
--- sub-rebase.sh
+++ sub-rebase.sh
@@ -4,20 +4,21 @@
 git status | grep "all conflicts fixed: run \"git rebase --continue\"" && git rebase --continue
 
 GIT_FOLDER=$(git rev-parse --show-toplevel)/.git
-DEFAULT_BRANCH=$([ -f ${GIT_FOLDER}/refs/heads/master ] && echo master || echo main)
+DEFAULT_BRANCH=$([ -f "${GIT_FOLDER}"/refs/heads/master ] && echo master || echo main)
 BASE=${1:-$DEFAULT_BRANCH}
 while true
 do
     # Find next commit to rebase to
     NEXT_COMMIT=
-    COMMON_ANCESTOR=$(git merge-base HEAD $BASE)
+    COMMON_ANCESTOR=$(git merge-base HEAD "$BASE")
 
     # Find all files touched by commits in the current branch
-    CHANGED_FILES=$(git log --pretty=format: --name-only $BASE..HEAD | sort -u)
+    CHANGED_FILES=$(git log --pretty=format: --name-only "$BASE"..HEAD | sort -u)
 
-    for COMMIT in $(git rev-list ..$BASE --reverse)
+    for COMMIT in $(git rev-list .."$BASE" --reverse)
     do
-        if [ $(git merge-base HEAD $COMMIT) != $COMMON_ANCESTOR ]
+        COMMIT_COMMON_ANCESTOR=$(git merge-base HEAD "$COMMIT")
+        if [ "$COMMIT_COMMON_ANCESTOR" != "$COMMON_ANCESTOR" ]
         then
             # Skipping $COMMIT as it is not strictly ahead of $BASE
             continue
@@ -29,7 +30,7 @@ do
         then
             # Checking if $COMMIT is a better candidate than $NEXT_COMMIT
 
-            CHANGES_TO_CUR=$(git diff --name-only $COMMON_ANCESTOR..$COMMIT | sort -u)
+            CHANGES_TO_CUR=$(git diff --name-only "$COMMON_ANCESTOR".."$COMMIT" | sort -u)
             OVERLAP=$(comm -12 <(echo "$CHANGED_FILES") <(echo "$CHANGES_TO_CUR"))
             if [ "$OVERLAP" != "" ]
             then
@@ -43,7 +44,7 @@ do
     done
 
     [ "$NEXT_COMMIT" == "" ] && echo "Done" && exit
-    NEXT_COMMIT_DATE=$(git show -s --format=%cd --date=short $NEXT_COMMIT)
-    echo Rebasing over $NEXT_COMMIT \($NEXT_COMMIT_DATE\)
-    git rebase $NEXT_COMMIT || exit 1
+    NEXT_COMMIT_DATE=$(git show -s --format=%cd --date=short "$NEXT_COMMIT")
+    echo Rebasing over "$NEXT_COMMIT" \("$NEXT_COMMIT_DATE"\)
+    git rebase "$NEXT_COMMIT" || exit 1
 done
